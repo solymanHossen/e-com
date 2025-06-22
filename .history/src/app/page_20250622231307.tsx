9e479a4f37844a7b1be48c6b47f22382259d6320
Md Solyman Hossen <coredeveloper.2013@gmail.com>
2025-06-22 23:32:41 +0600

    added

--- .history/src/app/page_20250622231307.tsx
+++ .history/src/app/page_20250622231307.tsx
@@ -1,11 +1,9 @@
 import { ModeToggle } from "@/components/toggle/mode-toggle";
-import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
       <ModeToggle />
-      <Button>this is my awestome buttom</Button>
     </div>
   );
 }
